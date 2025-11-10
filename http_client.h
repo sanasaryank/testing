b2272bbb3bbb4959510263c416217f6b8044bcba
Karen Sanasaryan <sanasaryan.k@gmail.com>
2025-11-10 13:18:43 +0400

    Add HTTPClient class for handling HTTP requests

---
+++ http_client.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+#include <boost/asio.hpp>
+#include <json_utils.h>
+#include <curl/curl.h>
+
+enum class HttpMethod { GET, POST, PUT, PATCH, DELETE, UNKNOWN };
+
+class TApiRequest;
+class HTTPClient {
+public:
+    static HttpMethod ToHttpMethod(const std::string& method) {
+        if (method == "GET") return HttpMethod::GET;
+        if (method == "POST") return HttpMethod::POST;
+        if (method == "PUT") return HttpMethod::PUT;
+        if (method == "DELETE") return HttpMethod::DELETE;
+        if (method == "PATCH") return HttpMethod::PATCH;
+        return HttpMethod::UNKNOWN;
+    }
+    struct Args {
+        TApiRequest* request{nullptr};
+        const std::string& id;
+        JSON& payload;
+    };
+    struct Result {
+        long http_status = 0;
+        std::string uri;
+        std::string response_body;
+        CURLcode curl_code = CURLE_OK;
+        std::string error_message;
+        [[nodiscard]] bool IsError() const { return 200 != http_status || CURLE_OK != curl_code; }
+        const std::string& Text() { return error_message.empty() ? response_body : error_message; }
+        JSON Json() { return JS::Parse(Text()); }
+    };
+    struct PurgeNodeResult {
+        std::string endpoint;
+        long status;
+        bool success;
+    };
+
+    struct PurgeResult {
+        std::string purgeId;
+        std::vector<PurgeNodeResult> nodes;
+        bool completeSuccess = false;
+    };
+
+    explicit HTTPClient(size_t thread_pool_size = 0);
+
+    void Get(const std::string& url,
+             const std::function<void(Result)>& callback,
+             bool async = true);
+
+
+    void Post(const std::string& url, const nlohmann::json& body,
+              const std::function<void(Result)>& callback,
+              bool async = true,
+              const std::vector<std::string>& headers = {});
+
+    // Sync version
+    static Result GetSync(const std::string& url);
+    static Result PostSync(const std::string& url, const nlohmann::json& body,
+                           const std::vector<std::string>& headers = {});
+
+    static HTTPClient::Result SendRequest(HttpMethod method,
+                                          const std::string& endpoint,
+                                          const std::vector<std::string>& path,
+                                          const std::string& data,
+                                          const std::vector<std::string>& headers,
+                                          const std::vector<std::pair<std::string, std::string>>& query = {},
+                                          bool reuse_connection = false,
+                                          const std::string& socket_path = "") noexcept;
+
+
+private:
+    std::unique_ptr<boost::asio::thread_pool> pool_;
+    static CURLSH* curl_share_;
+    static void ConfigurePost(CURL* curl, const std::string& data);
+    static void ConfigurePut(CURL* curl, const std::string& data);
+    static void ConfigureDelete(CURL* curl, const std::string& data);
+    static void ConfigureGet(CURL* curl);
+    static void ConfigurePatch(CURL* curl, const std::string& data);
+    static void ConfigurePurge(CURL* curl, const std::string& cacheCluster);
+};
